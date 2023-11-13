642e96c464308d84f5e24f51f4fa3866f6518595
SaiSithuPhyo <122963117+SaiSithuPhyo@users.noreply.github.com>
2023-11-13 17:14:33 +0700

    Add files via upload

---
+++ YourStudyBuddy/src/test/java/com/example/YourStudyBuddy/YourStudyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.YourStudyBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourStudyBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
